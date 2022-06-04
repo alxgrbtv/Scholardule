1d794f28c211b599afd3bf1f6f61ab661b37edcd
AlexanderGorbatov <alxgrbtv@gmail.com>
2022-06-04 08:46:17 +0300

    'Project basic structure' commit

--- app/src/main/java/com/example/scholardule/view/MainActivity.kt
+++ app/src/main/java/com/example/scholardule/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.scholardule
+package com.example.scholardule.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.scholardule.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/scholardule/view/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.scholardule.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.scholardule.R
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
